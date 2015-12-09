f7c6e359bc1a25e70b23e5fb6ed177c8594fe33b
Omar Mendoza <omaryahir@gmail.com>
2015-12-09 17:15:55 -0600

    Intro

---
+++ alumnos/scripts.js
@@ -0,0 +1,10 @@
+function AlumnosController($scope){
+    $scope.alumnos=[
+        {nombre:"Cuatemoc", telefono:"9717136182", nivel:"Primero" },
+        {nombre:"Raúl", telefono:"4234324182", nivel:"Segundo" },
+        {nombre:"Omar", telefono:"324234182", nivel:"Tercero" },
+        {nombre:"Mario", telefono:"2342346182", nivel:"Primero" },
+        {nombre:"Edgar", telefono:"23432471723", nivel:"Primero" },
+        {nombre:"Ramiro", telefono:"324234346565", nivel:"Primero" }
+    ];
+}
