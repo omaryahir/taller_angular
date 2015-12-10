8ea0a90c4d9508fd32f0b47a787744c6fc3e8eab
Omar Mendoza <omaryahir@gmail.com>
2015-12-09 18:47:20 -0600

    ADD: show hide form

--- alumnos/mis_scripts.js
+++ alumnos/mis_scripts.js
@@ -8,7 +8,17 @@ function StudentController($scope){
         {nombre:"Luis", telefono:"23432471723", nivel:"Primero" }
     ];
 
+    $scope.formVisibility=false;
+
     $scope.Save=function(){
-        $scope.alumnos.push({nombre:$scope.nuevoAlumno.nombre, telefono:$scope.nuevoAlumno.telefono, nivel:$scope.nuevoAlumno.nivel});
+        $scope.alumnos.push({nombre:$scope.nuevoAlumno.nombre, telefono:$scope.nuevoAlumno.telefono, nivel:$scope.nuevoAlumno.nivel}); 
+        $scope.formVisibility=false;
+        console.log($scope.formVisibility);
     }
+
+    $scope.ShowForm=function(){
+        $scope.formVisibility=true;
+        console.log($scope.formVisibility);
+    }
+
 }
